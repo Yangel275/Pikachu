fac42eb6ab9340077d8d503664a4052ac849a05b
Yangel275 <martinez.yangel@correo.unimet.edu.ve>
2024-04-02 22:54:03 -0400

    New

--- JavaApplication32/src/javaapplication32/JavaApplication32.java
+++ JavaApplication32/src/javaapplication32/JavaApplication32.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package pikachu;
+package javaapplication32;
 
 /**
  *
  * @author yangel
  */
-public class Pikachu {
+public class JavaApplication32 {
 
     /**
      * @param args the command line arguments
